aabb06bd992ff4eca2c6320fb3e65025c727f341
Paul Oyelabi <oyelabipaul@gmail.com>
2023-12-29 23:23:51 +0100

    [fix]: Corrected naming style
        - Formatted source files using go fmt utility.
        - Correct naming style to the go recommended naming style.

--- helpers/get_words.go
+++ helpers/get_words.go
@@ -4,66 +4,66 @@
 package main
 
 import (
-    "fmt"
-    "io"
-//    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"io"
+	//    "log"
+	"net/http"
+	"os"
 )
 
-func download_and_save(url string, save_path string, ch chan<- string) {
-    response, err := http.Get(url)
-    if err != nil {
-        ch<- fmt.Sprintf("Error fetching from %s.\nError:%v\n", url, err)
-        return
-    }
+func downloadAndSave(url string, savePath string, ch chan<- string) {
+	response, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprintf("Error fetching from %s.\nError:%v\n", url, err)
+		return
+	}
 
-    if response.StatusCode != http.StatusOK {
-        ch<- fmt.Sprintf("Error fetching from %s.\nError:%v\n",
-                        url, response.StatusCode)
-        return
-    }
+	if response.StatusCode != http.StatusOK {
+		ch <- fmt.Sprintf("Error fetching from %s.\nError:%v\n",
+			url, response.StatusCode)
+		return
+	}
 
-    save_file, err := os.OpenFile(save_path, os.O_RDWR|os.O_CREATE|os.O_TRUNC,
-                                0666)
-    if err != nil {
-        ch<- fmt.Sprintf("Error opening %q\n", save_path)
-        return
-    }
+	saveFile, err := os.OpenFile(savePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC,
+		0666)
+	if err != nil {
+		ch <- fmt.Sprintf("Error opening %q\n", save_path)
+		return
+	}
 
-    written_bytes, err := io.Copy(save_file, response.Body)
-    response.Body.Close()
-    if err != nil {
-        ch<- fmt.Sprintf("Error writing to: %q\n", save_path)
-        return
-    }
+	writtenBytes, err := io.Copy(saveFile, response.Body)
+	response.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("Error writing to: %q\n", savePath)
+		return
+	}
 
-    ch<- fmt.Sprintf("Wrote %d bytes to %q\n", written_bytes, save_path)
+	ch <- fmt.Sprintf("Wrote %d bytes to %q\n", writtenBytes, savePath)
 }
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Fprintf(os.Stderr, "usage:\n\tget_words SAVE_DIR\n")
-        os.Exit(1)
-    }
-    savePath := os.Args[1]
-    urlPrefix := "https://www.mso.anu.edu.au/~ralph/OPTED/v003/"
-    resourceLocations := []string{
-       "wb1913_a.html", "wb1913_b.html", "wb1913_c.html", "wb1913_d.html",
-       "wb1913_e.html", "wb1913_f.html", "wb1913_g.html", "wb1913_h.html",
-       "wb1913_i.html", "wb1913_j.html", "wb1913_k.html", "wb1913_l.html",
-       "wb1913_m.html", "wb1913_n.html", "wb1913_o.html", "wb1913_p.html",
-       "wb1913_q.html", "wb1913_r.html", "wb1913_s.html", "wb1913_t.html",
-       "wb1913_u.html", "wb1913_v.html", "wb1913_w.html", "wb1913_x.html",
-       "wb1913_y.html", "wb1913_z.html"}
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage:\n\tget_words SAVE_DIR\n")
+		os.Exit(1)
+	}
+	savePath := os.Args[1]
+	urlPrefix := "https://www.mso.anu.edu.au/~ralph/OPTED/v003/"
+	resourceLocations := []string{
+		"wb1913_a.html", "wb1913_b.html", "wb1913_c.html", "wb1913_d.html",
+		"wb1913_e.html", "wb1913_f.html", "wb1913_g.html", "wb1913_h.html",
+		"wb1913_i.html", "wb1913_j.html", "wb1913_k.html", "wb1913_l.html",
+		"wb1913_m.html", "wb1913_n.html", "wb1913_o.html", "wb1913_p.html",
+		"wb1913_q.html", "wb1913_r.html", "wb1913_s.html", "wb1913_t.html",
+		"wb1913_u.html", "wb1913_v.html", "wb1913_w.html", "wb1913_x.html",
+		"wb1913_y.html", "wb1913_z.html"}
 
-    ch := make(chan string)
+	ch := make(chan string)
 
-    for _, r := range resourceLocations {
-        go download_and_save(urlPrefix+r, savePath+r, ch)
-    }
+	for _, r := range resourceLocations {
+		go downloadAndSave(urlPrefix+r, savePath+r, ch)
+	}
 
-    for range resourceLocations {
-        fmt.Fprintf(os.Stderr, <-ch)
-    }
+	for range resourceLocations {
+		fmt.Fprintf(os.Stderr, <-ch)
+	}
 }
--- helpers/html_to_json.go
+++ helpers/html_to_json.go
@@ -5,114 +5,115 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "io/fs"
-    "io/ioutil"
-    "os"
-    "regexp"
-    "strings"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 )
 
 type Definition struct {
-    PartOfSpeech    string `json:"part_of_speech"`
-    WordDefinition  string  `json:"definition"`
+	PartOfSpeech   string `json:"part_of_speech"`
+	WordDefinition string `json:"definition"`
 }
+
 type DictEntity struct {
-    Word            string          `json:"word"`
-    Spellings       []string        `json:"alternate_spellings,omitempty"`
-    WordDefinitions []Definition    `json:"definitions"`
+	Word            string       `json:"word"`
+	Spellings       []string     `json:"alternate_spellings,omitempty"`
+	WordDefinitions []Definition `json:"definitions"`
 }
 
-func parseHTML(file_path string, ch chan<- string) {
-    opened_file, err := os.Open(file_path)
-    if err != nil {
-        ch<- fmt.Sprintf("Error opening %q\n", file_path)
-        return
-    }
+func parseHTML(filePath string, ch chan<- string) {
+	openedFile, err := os.Open(filePath)
+	if err != nil {
+		ch <- fmt.Sprintf("Error opening %q\n", filePath)
+		return
+	}
 
-    read_bytes, err := ioutil.ReadAll(opened_file)
-    if err != nil {
-        ch<- fmt.Sprintf("Error reading %q\n", file_path)
-        return
-    }
+	readBytes, err := ioutil.ReadAll(openedFile)
+	if err != nil {
+		ch <- fmt.Sprintf("Error reading %q\n", filePath)
+		return
+	}
 
-    pattern := regexp.MustCompile(
-        `(?m)<P><B>(?P<word>\w+)</B>\s+\(<I>(?P<pos>.*)</I>\)\s+(?P<def>.+)</P>$`)
+	pattern := regexp.MustCompile(
+		`(?m)<P><B>(?P<word>\w+)</B>\s+\(<I>(?P<pos>.*)</I>\)\s+(?P<def>.+)</P>$`)
 
-    template := []byte("$word|$pos|$def")
-    dict_entities := map[string]DictEntity{}
-    for _, submatches := range pattern.FindAllSubmatchIndex(read_bytes, -1) {
-        b := []byte{}
-        b = pattern.Expand(b, template, read_bytes, submatches)
-        splitted_bytes := bytes.Split(b, []byte("|"))
+	template := []byte("$word|$pos|$def")
+	dictEntities := map[string]DictEntity{}
+	for _, submatches := range pattern.FindAllSubmatchIndex(readBytes, -1) {
+		b := []byte{}
+		b = pattern.Expand(b, template, readBytes, submatches)
+		splittedBytes := bytes.Split(b, []byte("|"))
 
-        word := strings.ToLower(string(splitted_bytes[0]))
-        pos := string(splitted_bytes[1])
-        def := string(splitted_bytes[2])
-        if entity, ok := dict_entities[word]; ok {
-            entity.WordDefinitions = append(
-                entity.WordDefinitions,
-                Definition{
-                    PartOfSpeech: pos,
-                    WordDefinition: def,
-                },
-            )
-            dict_entities[word] = entity
-        } else {
-            dict_entities[word] = DictEntity{
-                Word: word,
-                WordDefinitions: []Definition{
-                    Definition{
-                        PartOfSpeech: pos,
-                        WordDefinition: def,
-                    },
-                },
-            }
-        }
-    }
-    encoding, err := json.MarshalIndent(dict_entities, "", "    ")
-    if err != nil {
-        ch<- fmt.Sprintf(
-            "Error occured while encoding file: %q to json.\n",
-            file_path)
-    }
+		word := strings.ToLower(string(splittedBytes[0]))
+		pos := string(splittedBytes[1])
+		def := string(splittedBytes[2])
+		if entity, ok := dictEntities[word]; ok {
+			entity.WordDefinitions = append(
+				entity.WordDefinitions,
+				Definition{
+					PartOfSpeech:   pos,
+					WordDefinition: def,
+				},
+			)
+			dictEntities[word] = entity
+		} else {
+			dictEntities[word] = DictEntity{
+				Word: word,
+				WordDefinitions: []Definition{
+					Definition{
+						PartOfSpeech:   pos,
+						WordDefinition: def,
+					},
+				},
+			}
+		}
+	}
+	encoding, err := json.MarshalIndent(dict_entities, "", "    ")
+	if err != nil {
+		ch <- fmt.Sprintf(
+			"Error occured while encoding file: %q to json.\n",
+			file_path)
+	}
 
-    out_file := strings.TrimSuffix(file_path, ".html")+".json"
-    save_file, err := os.OpenFile(out_file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-    if err != nil {
-        ch<- fmt.Sprintf("Error opening output file: %q\n", out_file)
-        return
-    }
+	outFile := strings.TrimSuffix(filePath, ".html") + ".json"
+	saveFile, err := os.OpenFile(outFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		ch <- fmt.Sprintf("Error opening output file: %q\n", outFile)
+		return
+	}
 
-    written_bytes, err := save_file.WriteString(fmt.Sprintf("%s\n", encoding))
-    if err != nil {
-        ch<- fmt.Sprintln("Error writing to output file.")
-        return
-    }
+	writtenBytes, err := saveFile.WriteString(fmt.Sprintf("%s\n", encoding))
+	if err != nil {
+		ch <- fmt.Sprintln("Error writing to output file.")
+		return
+	}
 
-    ch<- fmt.Sprintf("Wrote %d bytes to %q\n", written_bytes, out_file)
+	ch <- fmt.Sprintf("Wrote %d bytes to %q\n", writtenBytes, outFile)
 }
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Fprintf(os.Stderr, "usage:\n\thtml_to_json HTML_DIR\n")
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage:\n\thtml_to_json HTML_DIR\n")
+		os.Exit(1)
+	}
 
-    filesDir := os.DirFS(os.Args[1])
-    htmlFiles, err := fs.Glob(filesDir, "wb1913_*.html")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error searching directory %q\n", os.Args[1])
-        os.Exit(1)
-    }
-    ch := make(chan string)
-    for _, f := range htmlFiles {
-        go parseHTML(os.Args[1]+f, ch)
-    }
+	filesDir := os.DirFS(os.Args[1])
+	htmlFiles, err := fs.Glob(filesDir, "wb1913_*.html")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error searching directory %q\n", os.Args[1])
+		os.Exit(1)
+	}
+	ch := make(chan string)
+	for _, f := range htmlFiles {
+		go parseHTML(os.Args[1]+f, ch)
+	}
 
-    for range htmlFiles {
-        fmt.Fprintf(os.Stderr, <-ch)
-    }
+	for range htmlFiles {
+		fmt.Fprintf(os.Stderr, <-ch)
+	}
 }
