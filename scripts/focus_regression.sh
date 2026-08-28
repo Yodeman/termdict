c29250d1a5210cd58d0c12fc710677a57cadb149
Paul Oyelabi <oyelabipaul@gmail.com>
2026-08-28 20:02:56 +0100

    chore(qa): remove the Python/shell e2e scripts replaced by Go tests

--- scripts/focus_regression.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Shell wrapper for the scripted-pty focus regression suite
-# (scripts/pty_focus.py). See that file for what is covered.
-set -eu
-cd "$(dirname "$0")/.."
-exec python3 scripts/pty_focus.py all
